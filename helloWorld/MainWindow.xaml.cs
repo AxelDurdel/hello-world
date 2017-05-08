1de1205064642ed667fd73f405e0c49c55ba453d
Axel Durdel <axel.durdel@gmx.de>
2017-05-08 21:50:30 +0200

    test vom rechner 2

--- helloWorld/MainWindow.xaml.cs
+++ helloWorld/MainWindow.xaml.cs
@@ -27,8 +27,8 @@ namespace helloWorld
 			label.Content = "Hello World";
 			label.Background = Brushes.LightBlue;
 			label.Background = Brushes.Red;
-	    	label.Visibility = Visibility.Visible;
-			//Test vom Rechner
+			label.Visibility = Visibility.Visible;
+			//Test vom Rechner 2
 		}
 	}
 }
